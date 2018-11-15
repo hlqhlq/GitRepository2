7140c6cef960af4ba1cac159137eac1530b4f446
HLQ <heliqingwork@hotmail.com>
2018-11-15 23:08:35 +0800

    this first eclipse commit

---
+++ TestGit/src/main/java/com/TestGit/Test.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package com.TestGit;
+
+/**
+ * @author HLQ
+ * @date  2018Äê11ÔÂ15ÈÕ
+ * @version 1.0
+ */
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println(1);
+	}
+}
